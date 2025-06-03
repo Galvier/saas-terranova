4768531167759a7c7a778d795f10ca11469214d1
Galvier <jonathangalvier@gmail.com>
2025-06-03 14:38:11 -0300

    feat: ajuste logo

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -5,7 +5,7 @@ const AppLogo = ({ className }: { className?: string }) => {
   return (
     <div className={`flex items-center justify-center gap-3 ${className}`}>
       <img 
-        src="/lovable-uploads/93cecebb-51e3-4cec-b407-4a6ea4c4f9d3.png" 
+        src="/img-uploads/93cecebb-51e3-4cec-b407-4a6ea4c4f9d3.png" 
         alt="Terranova Logo" 
         className="h-12 w-auto"
       />
